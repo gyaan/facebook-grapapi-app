68d42b7135f92d08bc386db3120ef84635ddbc97
gyani <gyani@gyani-HP-630-Notebook-PC.(none)>
2013-09-09 20:37:05 +0530

    test

--- index.php
+++ index.php
@@ -68,7 +68,8 @@ if ($user_id) {
 
     // This fetches 4 of your friends.
 //  $friends = idx($facebook->api('me/friends?fields=work&access_token='.$accessToken.''), 'data', array());
-    $friends = $facebook->api('me/friends?fields=id,name,work&access_token='.$accessToken.'');
+//    $friends = $facebook->api('me/friends?fields=id,name,work&access_token='.$accessToken.'');
+    $friends = $facebook->api('me/friends?fields=id,name,work');
 
 
     $FriendHaveTitle = array();
